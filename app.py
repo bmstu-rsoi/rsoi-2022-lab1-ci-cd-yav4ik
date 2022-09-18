b8d7c021109d3bf13c9431aebb6c544e2527717d
yav4ik <dmitrsilin1@yandex.ru>
2022-09-19 00:02:06 +0300

    test heroku build

--- app.py
+++ app.py
@@ -1,24 +1,55 @@
-from flask import Flask
-#from flask_restful import Api, Resource, reqparse
+from flask import Flask, request
+from src.person import Person
 
 app = Flask(__name__)
 
 
-@app.route('/persons/<int:person_id>', methods=['GET'])
-def get_persons(person_id):
-    return person_id, 200
+@app.route('/persons/<int:person_id>', methods=["GET"])
+def get_person(person_id):
+    person = Person()
+    person_json = person.get_person(person_id)
+    if person_json is None:
+        return f"person with id {person_id} not found", 404
+    return person_json
 
-# words = ["Hello", "world"]
-#
-#
-# class IDResource(Resource):
-#     def get(self):
-#         return words[0] + "" + words[1], 200
-#
-#
-# api.add_resource(IDResource, "/id", "/id/", "/id/<int:id>")
+
+@app.route('/persons', methods=["GET"])
+def get_all_person():
+    person = Person()
+    persons_json = person.get_all_persons()
+    if persons_json is None:
+        return f"there's no persons", 404
+    return persons_json
+
+
+@app.route('/persons', methods=["POST"])
+def post_person():
+    new_person = request.json
+    person = Person()
+    person_json = person.create_person(new_person)
+    if person_json is None:
+        return "something wrong", 500
+    app.redirect(location='/persons/new_person["personID"]', code=201)
+
+
+@app.route('/persons/<int:person_id>', methods=["PATCH"])
+def patch_person(person_id):
+    new_person = request.json
+    person = Person()
+    person_json = person.get_person(person_id)
+    if person_json is None:
+        return "something wrong", 500
+    return person.update_person(new_person, person_id)
+
+
+@app.route('/persons/<int:person_id>', methods=["DELETE"])
+def delete_person(person_id):
+    person = Person()
+    person_json = person.get_person(person_id)
+    if person_json is None:
+        return f"person with id {person_id} not found", 404
+    return f"person with id {person_id} deleted"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
---
+++ src/person.py
@@ -0,0 +1,80 @@
+import json
+
+from src.databaseRequests import DatabaseRequests
+
+
+class Person:
+    def __init__(self):
+        self.request_db = DatabaseRequests()
+        self.person = {
+            "personId": None,
+            "name": None,
+            "address": None,
+            "work": None,
+            "age": None
+        }
+
+    def person_from_tuple(self, tuple_db):
+        if len(tuple_db) != 5:
+            raise Exception("tuple length is not 5")
+
+        self.person = {
+            "personId": tuple_db[0],
+            "name": tuple_db[1],
+            "address": tuple_db[2],
+            "work": tuple_db[3],
+            "age": tuple_db[4]
+        }
+
+    def get_person(self, person_id):
+        tuple_db = self.request_db.get_person(person_id)
+        if not tuple_db:
+            return None
+
+        self.person_from_tuple(tuple_db)
+        return json.dumps(self.person)
+
+    def get_all_persons(self):
+        tuple_db = self.request_db.get_all_persons()
+        persons = []
+        if not tuple_db:
+            return None
+
+        for i in tuple_db:
+            self.person_from_tuple(i)
+            persons.append(self.person)
+
+        return json.dumps(persons)
+
+    def create_person(self, person):
+        self.request_db.add_person(person)
+        tuple_db = self.request_db.get_person(person["personId"])
+        if not tuple_db:
+            return None
+
+        self.person_from_tuple(tuple_db)
+        if self.person != person:
+            return None
+
+        return json.dumps(self.person)
+
+    def update_person(self, new_person, person_id):
+        tuple_db = self.request_db.get_person(person_id)
+        if not tuple_db:
+            return None
+
+        self.person_from_tuple(tuple_db)
+        self.person.update(new_person)
+        self.request_db.update_person(self.person)
+        return 1
+
+    def delete_person(self, person_id):
+        tuple_db = self.request_db.get_person(person_id)
+        if not tuple_db:
+            return None
+
+        self.request_db.delete_person(person_id)
+        return 1
+
+
+
