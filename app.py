6967cba733de85e426a2fe87a1cda4db85a26b6a
yav4ik <dmitrsilin1@yandex.ru>
2022-09-18 18:07:13 +0300

    flask test

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+#from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
-api = Api(app)
-
-words = ["Hello", "world"]
 
 
-class IDResource(Resource):
-    def get(self):
-        return words[0] + "" + words[1], 200
+@app.route('/persons/<int:person_id>', methods=['GET'])
+def get_persons(person_id):
+    return person_id, 200
 
-
-api.add_resource(IDResource, "/id", "/id/", "/id/<int:id>")
+# words = ["Hello", "world"]
+#
+#
+# class IDResource(Resource):
+#     def get(self):
+#         return words[0] + "" + words[1], 200
+#
+#
+# api.add_resource(IDResource, "/id", "/id/", "/id/<int:id>")
 
 
 if __name__ == '__main__':
